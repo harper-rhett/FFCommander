0e68729315adc67ffc200f00f6abf4385485a41c
Harper Rhett <harperrhett@gmail.com>
2025-04-25 17:46:19 -0500

    Added codec testing

--- FFConvert.cs
+++ FFConvert.cs
@@ -16,7 +16,8 @@ public class FFConvert : ProcessRunner
 		VideoCodec videoCodec = VideoCodec.None,
 		PixelFormat pixelFormat = PixelFormat.None,
 		AudioCodec audioCodec = AudioCodec.None,
-		bool stripAudio = false
+		bool stripAudio = false,
+		string customExpressions = ""
 	)
 	{
 		// Set defaults
@@ -24,11 +25,27 @@ public class FFConvert : ProcessRunner
 		if (videoCodec == VideoCodec.None) videoCodec = videoFormat.GetDefaultVideoCodec();
 		if (pixelFormat == PixelFormat.None) pixelFormat = videoCodec.GetDefaultPixelFormat();
 		if (audioCodec == AudioCodec.None) audioCodec = videoFormat.GetDefaultAudioCodec();
+
+		// up next:
+		// create filter builder
+		// consider a universal codec compression system
 	}
 
 	private VideoFormat ExtractVideoFormat(string inputMediaPath)
 	{
 		string extension = Path.GetExtension(inputMediaPath);
 		return VideoFormats.GetFromExtension(extension);
+	}
+
+	public bool DoesVideoCodecRun(VideoCodec videoCodec)
+	{
+		// Test codec
+		string codecDefinition = videoCodec.GetDefinition();
+		string output = RunWithError($"-f lavfi -i testsrc -t 0.25 -c:v {codecDefinition} -f null -", out bool didSucceed);
+		if (!didSucceed) throw new Exception("Codec test failed.");
+
+		// Test codec output
+		bool doesRun = !output.Contains("error while opening encoder", StringComparison.OrdinalIgnoreCase);
+		return doesRun;
 	}
 }
