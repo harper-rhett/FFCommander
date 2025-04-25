d517e1b1f57a2d464205e0bae6e1a70f723268b2
Harper Rhett <harperrhett@gmail.com>
2025-04-25 18:25:39 -0500

    Started work on filters

--- FFConvert.cs
+++ FFConvert.cs
@@ -17,6 +17,7 @@ public class FFConvert : ProcessRunner
 		PixelFormat pixelFormat = PixelFormat.None,
 		AudioCodec audioCodec = AudioCodec.None,
 		bool stripAudio = false,
+		Filters filters = null,
 		string customExpressions = ""
 	)
 	{
@@ -27,7 +28,6 @@ public class FFConvert : ProcessRunner
 		if (audioCodec == AudioCodec.None) audioCodec = videoFormat.GetDefaultAudioCodec();
 
 		// up next:
-		// create filter builder
 		// consider a universal codec compression system
 	}
 
@@ -47,5 +47,42 @@ public class FFConvert : ProcessRunner
 		// Test codec output
 		bool doesRun = !output.Contains("error while opening encoder", StringComparison.OrdinalIgnoreCase);
 		return doesRun;
+	}
+}
+
+public class Filters
+{
+	private List<string> expressions = new();
+
+	public void AddExpression(string expression) => expressions.Add(expression);
+
+	// Need to add flags!
+	// Gif uses lanczos
+	// Don't need to specify for other file types
+
+	public void AddScale(int width, int height)
+	{
+		AddExpression($"scale={width}:{height}");
+	}
+
+	public void AddFrameRate(double frameRate)
+	{
+		AddExpression($"fps={frameRate}");
+	}
+
+	public void AddSubtitles(string subtitlesPath)
+	{
+		string sanitizedPath = @$"{subtitlesPath.Replace("\\", "/").Replace(":", "\\:")}";
+		AddExpression($"subtitles={sanitizedPath}");
+	}
+
+	public void AddPaletteLimit(int maxColors)
+	{
+		//split[a][b];[a]palettegen=max_colors=[max colors][p];[b][p]paletteuse
+	}
+
+	public string GetFinalExpression()
+	{
+		return $"-vf \"{string.Join(",", expressions)}\"";
 	}
 }
